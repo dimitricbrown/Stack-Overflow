2d6f9d9747ce3f04d554e1128f7ac4b642b663a0
dimitricbrown <dimitricbrown@outlook.com>
2023-05-13 08:36:50 -0500

    Added styling to the navbars

--- components/SideNavBar.js
+++ components/SideNavBar.js
@@ -5,25 +5,27 @@ import { Container, Nav } from 'react-bootstrap';
 
 export default function SideNavBar() {
   return (
-    <Container className="sidebar">
-      <Nav className="flex-column">
-        {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-        <Link passHref href="/">
-          <Nav.Link>Questions</Nav.Link>
-        </Link>
-        <Link passHref href="/company">
-          <Nav.Link>Companies</Nav.Link>
-        </Link>
-        <Link passHref href="/company/new">
-          <Nav.Link>Create Company</Nav.Link>
-        </Link>
-        <Link passHref href="/profile">
-          <Nav.Link>Profile</Nav.Link>
-        </Link>
-        <Link passHref href="/users">
-          <Nav.Link>Users</Nav.Link>
-        </Link>
-      </Nav>
-    </Container>
+    <div className="sidebar">
+      <Container className="sidebar-container">
+        <Nav className="sidebar-flex-column-nav">
+          {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
+          <Link className="ps-relative" passHref href="/">
+            <Nav.Link className="nav-topic">Questions</Nav.Link>
+          </Link>
+          <Link className="ps-relative" passHref href="/company">
+            <Nav.Link className="nav-topic">Companies</Nav.Link>
+          </Link>
+          <Link className="ps-relative" passHref href="/company/new">
+            <Nav.Link className="nav-topic">Create Company</Nav.Link>
+          </Link>
+          <Link className="ps-relative" passHref href="/profile">
+            <Nav.Link className="nav-topic">Profile</Nav.Link>
+          </Link>
+          <Link className="ps-relative" passHref href="/users">
+            <Nav.Link className="nav-topic">Users</Nav.Link>
+          </Link>
+        </Nav>
+      </Container>
+    </div>
   );
 }
