9f9b064e5967bdc6b582073f6d1395459777bc58
Beilei Zhu <beileibud@gmail.com>
2023-05-12 20:52:20 -0500

    code back

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap';
